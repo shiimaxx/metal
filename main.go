7e010bd1191dc7d428ca6a37af46213bfce0e00a
Takatada Yoshima <yoshima@topotal.com>
2022-04-03 16:09:08 +0900

    wip

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 type Metric struct {
@@ -22,15 +23,28 @@ type Collector struct {
 
 func (c *Collector) Run(ctx context.Context, send chan<- []Metric) {
 	for i := 0; i < 10; i++ {
-		stat, err := cpu.PercentWithContext(ctx, time.Second, false)
+		timestamp := time.Now()
+
+		cpustat, err := cpu.PercentWithContext(ctx, time.Second, false)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+
+		memstat, err := mem.VirtualMemory()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s\n", err)
 		}
 
 		c.Metrics = append(c.Metrics, Metric{
 			Name:      "cpu.user",
-			Timestamp: time.Now(),
-			Value:     stat[0],
+			Timestamp: timestamp,
+			Value:     cpustat[0],
+		})
+
+		c.Metrics = append(c.Metrics, Metric{
+			Name:      "memory",
+			Timestamp: timestamp,
+			Value:     memstat.UsedPercent,
 		})
 	}
 	send <- c.Metrics
