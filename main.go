7459c6508e5351674d6df978294a6cf6f1681284
Takatada Yoshima <yoshima@topotal.com>
2022-04-09 09:57:19 +0900

    wip

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"time"
@@ -12,7 +11,6 @@ import (
 	"github.com/shirou/gopsutil/v3/mem"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch"
 	"github.com/aws/aws-sdk-go-v2/service/cloudwatch/types"
 )
@@ -33,7 +31,12 @@ func (c *CPUCollector) Collect(done <-chan struct{}, send chan<- []Metric) {
 	var metrics []Metric
 	ticker := time.NewTicker(time.Second)
 
-	_, err := cpu.Percent(0, false)
+	var previousCPUStats []cpu.TimesStat
+	var latestCPUStats []cpu.TimesStat
+	var err error
+
+	// for countern
+	latestCPUStats, err = cpu.TimesWithContext(context.TODO(), false)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
@@ -41,15 +44,78 @@ func (c *CPUCollector) Collect(done <-chan struct{}, send chan<- []Metric) {
 	for {
 		select {
 		case t := <-ticker.C:
-			cpuuser, err := cpu.Percent(0, false)
+			previousCPUStats = latestCPUStats
+			latestCPUStats, err = cpu.TimesWithContext(context.TODO(), false)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err)
 			}
 
+			previous := previousCPUStats[0]
+			latest := latestCPUStats[0]
+
+			diff := cpu.TimesStat{
+				CPU:       latest.CPU,
+				User:      latest.User - previous.User,
+				System:    latest.System - previous.System,
+				Idle:      latest.Idle - previous.Idle,
+				Nice:      latest.Nice - previous.Nice,
+				Iowait:    latest.Iowait - previous.Iowait,
+				Irq:       latest.Irq - previous.Irq,
+				Softirq:   latest.Softirq - previous.Softirq,
+				Steal:     latest.Steal - previous.Steal,
+				Guest:     latest.Guest - previous.Guest,
+				GuestNice: latest.GuestNice - previous.GuestNice,
+			}
+
 			metrics = append(metrics, Metric{
-				Name:      "cpu",
+				Name:      "CPUUser",
 				Timestamp: t,
-				Value:     cpuuser[0],
+				Value:     diff.User / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUSystem",
+				Timestamp: t,
+				Value:     diff.System / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUIdle",
+				Timestamp: t,
+				Value:     diff.Idle / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUNice",
+				Timestamp: t,
+				Value:     diff.Nice / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUIowait",
+				Timestamp: t,
+				Value:     diff.Iowait / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUIrq",
+				Timestamp: t,
+				Value:     diff.Irq / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUSoftirq",
+				Timestamp: t,
+				Value:     diff.Softirq / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUSteal",
+				Timestamp: t,
+				Value:     diff.Steal / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUGuest",
+				Timestamp: t,
+				Value:     diff.Guest / diff.Total() * 100,
+			})
+			metrics = append(metrics, Metric{
+				Name:      "CPUGuestNice",
+				Timestamp: t,
+				Value:     diff.GuestNice / diff.Total() * 100,
 			})
 		case <-done:
 			send <- metrics
@@ -73,7 +139,7 @@ func (m *MemCollector) Collect(done <-chan struct{}, send chan<- []Metric) {
 			}
 
 			metrics = append(metrics, Metric{
-				Name:      "memory",
+				Name:      "MemoryUsed",
 				Timestamp: t,
 				Value:     memstat.UsedPercent,
 			})
@@ -157,14 +223,14 @@ func main() {
 	}
 	go collector.Run(ctx, metricCh)
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// client := cloudwatch.NewFromConfig(cfg)
+	// publisher := AmazonCloudWatchPublisher{Client: client}
 
-	client := cloudwatch.NewFromConfig(cfg)
-
-	publisher := AmazonCloudWatchPublisher{Client: client}
+	publisher := StdoutPublisher{}
 	go publisher.Run(ctx, metricCh)
 
 	<-ctx.Done()
